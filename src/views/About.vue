90bbe58a14b1b870c029d50109132c1578d59943
uguisu-an <arcana278@gmail.com>
2019-06-14 18:28:12 +0900

    addressを削除した

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <pre>{{ address }}</pre>
-    <v-address-input v-model="address" />
-
     <pre>{{ tasks }}</pre>
     <v-task-list-input v-model="tasks" />
   </div>
@@ -10,23 +7,14 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import VueAddressInput from "../components/VueAddressInput.vue";
 import VueTaskListInput from "../components/VueTaskListInput.vue";
 
 @Component({
   components: {
-    "v-address-input": VueAddressInput,
     "v-task-list-input": VueTaskListInput
   }
 })
 export default class Home extends Vue {
-  address = {
-    postcode: "123-4567",
-    address: "",
-    latitude: 0.0,
-    longitude: 0.0
-  };
-
   tasks = [
     {
       id: 1,
@@ -39,10 +27,5 @@ export default class Home extends Vue {
       description: "hi hi hi hi !"
     }
   ];
-
-  // input() {
-  //   console.info("test");
-  //   // this.$set(this.tasks, i, task);
-  // }
 }
 </script>
